49af90b858aeff2e19cff003631aa69c1c16f976
George <lwhittington2231@gmail.com>
2023-04-24 00:10:58 -0400

    Created function to simualte one round of game

--- script.js
+++ script.js
@@ -2,8 +2,48 @@
 
 // Date: 04-23-2023
 
+// Computes the computer choice
 function getComputerChoice() {
     const CHOICE = ["rock", "paper", "scissors"];
 
     return CHOICE[Math.floor(Math.random() * 3)];
+}
+
+// Plays a round of rock, paper, scissors and declares a winner
+function round(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return `Tie Game! You both chose ${playerSelection}`;
+    }
+
+    else if (playerSelection === "rock"){
+        if (computerSelection === "paper"){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+
+        else {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    }
+
+    else if (playerSelection === "paper"){
+        if (computerSelection === "scissors"){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+
+        else {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    }
+    else if (playerSelection === "scissors"){
+        if (computerSelection === "rock"){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+
+        else {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    }
 }
